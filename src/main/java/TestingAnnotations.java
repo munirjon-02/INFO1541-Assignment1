46f7690444ac55f50122db43301f11cb1a8a2d89
munirjon-02 <azimovmunirjon@gmail.com>
2025-09-09 20:46:25 -0500

    Update TestingAnnotations.java

--- src/main/java/TestingAnnotations.java
+++ src/main/java/TestingAnnotations.java
@@ -2,41 +2,58 @@ import EmployeeObjects.CommissionEmployee;
 import EmployeeObjects.HourlyEmployee;
 import EmployeeObjects.SalaryEmployee;
 
-import java.lang.reflect.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class TestingAnnotations {
-    public static void main(String[] args) throws Exception {
-        HourlyEmployee h = new HourlyEmployee("Tony", "Stark", 5749, "Service", "Lead Service Manager", 32.85);
-        SalaryEmployee s = new SalaryEmployee("Steve", "Rodgers", 3781, "Sales", "Manager", 64325);
-        CommissionEmployee c = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Rep", 0.0265);
+    public static void main(String[] args) {
+        // Create employee objects
+        HourlyEmployee hourly = new HourlyEmployee("Tony", "Stark", 5749, "Service", "Lead Service Manager", 32.85);
+        SalaryEmployee salary = new SalaryEmployee("Steve", "Rodgers", 3781, "Sales", "Manager", 64325);
+        CommissionEmployee commission = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Rep", 0.0265);
 
-        Object[] employees = {h, s, c};
-        int annotatedCount = 0;
+        Object[] employees = {hourly, salary, commission};
+        int annotatedClasses = 0;
 
         for (Object emp : employees) {
-            Class<?> cls = emp.getClass();
+            Class<?> clazz = emp.getClass();
 
-            if (cls.isAnnotationPresent(EmployeeType.class)) {
-                annotatedCount++;
+            // Class-level annotation
+            if (clazz.isAnnotationPresent(EmployeeType.class)) {
+                EmployeeType type = clazz.getAnnotation(EmployeeType.class);
+                System.out.println(clazz.getSimpleName() + " EmployeeType: " + type.type());
+                annotatedClasses++;
             }
 
-            // Fields
-            for (Field f : cls.getDeclaredFields()) {
-                if (f.isAnnotationPresent(PayRate.class)) {
-                    f.setAccessible(true);
-                    System.out.println(cls.getSimpleName() + " pay rate: $" + f.get(emp));
+            // Field-level annotation
+            for (Field field : clazz.getDeclaredFields()) {
+                if (field.isAnnotationPresent(PayRate.class)) {
+                    field.setAccessible(true);
+                    try {
+                        PayRate payRate = field.getAnnotation(PayRate.class);
+                        System.out.println(clazz.getSimpleName() + " pay rate (" + payRate.type() + "): $" + field.get(emp));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
 
-            // Methods
-            for (Method m : cls.getDeclaredMethods()) {
-                if (m.isAnnotationPresent(WeeklyPayCalculator.class)) {
-                    m.setAccessible(true);
-                    System.out.println(cls.getSimpleName() + " weekly pay: " + m.invoke(emp));
+            // Method-level annotation
+            for (Method method : clazz.getDeclaredMethods()) {
+                if (method.isAnnotationPresent(WeeklyPayCalculator.class)) {
+                    method.setAccessible(true);
+                    try {
+                        Object result = method.invoke(emp);
+                        System.out.println(clazz.getSimpleName() + " weekly pay: " + result);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
+
+            System.out.println("----------------------------");
         }
 
-        System.out.println("You have " + annotatedCount + " employee types");
+        System.out.println("You have " + annotatedClasses + " employee types annotated.");
     }
 }
